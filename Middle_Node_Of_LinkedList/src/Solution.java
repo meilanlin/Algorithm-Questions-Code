8cad606824923cc73d902d31ca0b9886033240f4
meilanlin <victorlizonglin@gmail.com>
2019-03-04 14:33:22 -0600

    linkedlist

---
+++ Middle_Node_Of_LinkedList/src/Solution.java
@@ -0,0 +1,25 @@
+class ListNode {
+	public int value;
+	public ListNode next;
+
+	public ListNode(int value) {
+		this.value = value;
+		next = null;
+	}
+}
+
+public class Solution {
+	public ListNode middleNode(ListNode head) {
+		if (head == null || head.next == null) {
+			return head;
+		}
+		// Write your solution here
+		ListNode slow = head;
+		ListNode fast = head;
+		while (fast.next != null && fast.next.next != null) {
+			slow = slow.next;
+			fast = fast.next.next;
+		}
+		return slow;
+	}
+}
