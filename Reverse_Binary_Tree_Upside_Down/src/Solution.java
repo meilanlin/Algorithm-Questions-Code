59955fe855f77bbd6d33e9ce00e7ac5e0e78e3c9
meilanlin <victorlizonglin@gmail.com>
2019-07-03 11:20:56 -0500

    tree

---
+++ Reverse_Binary_Tree_Upside_Down/src/Solution.java
@@ -0,0 +1,75 @@
+/*
+Given a binary tree where all the right nodes are leaf nodes, flip it upside down and turn it into a tree with left leaf nodes as the root.
+
+Examples
+
+        1
+
+      /    \
+
+    2        5
+
+  /   \
+
+3      4
+
+is reversed to
+
+        3
+
+      /    \
+
+    2        4
+
+  /   \
+
+1      5
+
+How is the binary tree represented?
+
+We use the pre order traversal sequence with a special symbol "#" denoting the null node.
+
+For Example:
+
+The sequence [1, 2, #, 3, 4, #, #, #] represents the following binary tree:
+
+    1
+
+  /   \
+
+ 2     3
+
+      /
+
+    4
+https://app.laicode.io/app/problem/178
+ */
+
+
+/**
+ * public class TreeNode {
+ *   public int key;
+ *   public TreeNode left;
+ *   public TreeNode right;
+ *   public TreeNode(int key) {
+ *     this.key = key;
+ *   }
+ * }
+ */
+public class Solution {
+  public TreeNode reverse(TreeNode root) {
+    // base case
+    if(root == null || root.left == null){
+      return root;
+    }
+
+    TreeNode node = reverse(root.left);
+    root.left.right = root.right;
+    
+    root.left.left = root;
+    root.left = null;
+    root.right = null;
+
+    return node;
+  }
+}
